863c12aa4748f7d5e5bae26db7d8f6c371ee32e2
Elias Mason <eliasjmason@gmail.com>
2017-08-27 20:46:42 -0700

    Removes aria attributes for testing

--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import GuessForm from './guess-form';
 
 import './guess-section.css';
 
 export default function GuessSection(props) {
     return (
-        <section aria-live="polite" role="status" aria-atomic="true">
+        <section>
             <a href="#userGuess"><h2 id="feedback" >{props.feedback}</h2></a>
             <GuessForm onGuess={props.onGuess} />
         </section>
