23816c7990d660ff1a548ebc28e4ac4cce8aa5a8
Elias Mason <eliasjmason@gmail.com>
2017-08-27 21:42:48 -0700

    Flattens guess section and guess form into one component

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
+import './guess-section.css';
 import './guess-form.css';
 
 export default class GuessForm extends React.Component {
+
     onGuess(event) {
         event.preventDefault();
+
+        console.log(this.props)
 
         if (this.props.onGuess) {
             const value = this.input.value;
             this.props.onGuess(value);
         }
         this.input.value = '';
+        this.feedbackLink.focus();
     }
 
     render() {
         return (
             <form onSubmit={e => this.onGuess(e)}>
+                <a href="#userGuess" 
+                ref={feedbackLink => this.feedbackLink = feedbackLink}>
+                    <h2 id="feedback" >{this.props.feedback}</h2>
+                </a>
                 <input type="number" name="userGuess" id="userGuess"
                     className="text" min="1" max="100" maxLength="3" autoComplete="off" aria-label="Enter a number"
                     aria-describedby="feedback" 
--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import GuessForm from './guess-form';
 
-import './guess-section.css';
+
 
 export default function GuessSection(props) {
     return (
         <section>
-            <a href="#userGuess"><h2 id="feedback" >{props.feedback}</h2></a>
+            
             <GuessForm onGuess={props.onGuess} />
         </section>
     );
