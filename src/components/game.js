1c53c96f34d7a9797dd04c3cdb821455058c8055
Elias Mason <eliasjmason@gmail.com>
2017-08-27 23:14:22 -0700

    Beautifies all files

--- src/components/game.js
+++ src/components/game.js
@@ -1,88 +1,89 @@
-import React from 'react';
+import React from "react";
 
-import Header from './header';
-import GuessSection from './guess-section';
-import StatusSection from './status-section';
-import InfoModal from './info-modal';
+import Header from "./header";
+import GuessSection from "./guess-section";
+import StatusSection from "./status-section";
+import InfoModal from "./info-modal";
 
 export default class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            guesses: [],
-            feedback: 'Make your guess!',
-            correctAnswer: Math.round(Math.random() * 100),
-            modalIsOpen: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      guesses: [],
+      feedback: "Make your guess!",
+      correctAnswer: Math.round(Math.random() * 100),
+      modalIsOpen: false
+    };
+  }
 
-    newGame() {
-        this.setState({
-            guesses: [],
-            feedback: 'Make your guess!',
-            correctAnswer: Math.floor(Math.random() * 100) + 1,
-        });
-    }
+  newGame() {
+    this.setState({
+      guesses: [],
+      feedback: "Make your guess!",
+      correctAnswer: Math.floor(Math.random() * 100) + 1
+    });
+  }
 
-    guess(guess) {
-        guess = parseInt(guess, 10);
-        if (isNaN(guess)) {
-            this.setState({
-                feedback: 'Please enter a valid number'
-            });
-            return;
-        }
-
-        const difference = Math.abs(guess - this.state.correctAnswer);
-
-        let feedback;
-        if (difference >= 50) {
-            feedback = 'You\'re Ice Cold...';
-        }
-        else if (difference >= 30) {
-            feedback = 'You\'re Cold...';
-        }
-        else if (difference >= 10) {
-            feedback = 'You\'re Warm';
-        }
-        else if (difference >= 1) {
-            feedback = 'You\'re Hot!';
-        }
-        else {
-            feedback = 'You got it!';
-        }
-
-        this.setState({
-            feedback,
-            guesses: [...this.state.guesses, guess]
-        });
-
-        document.title = (feedback) ? `${feedback} | Hot or Cold` : 'Hot or Cold';
-    }
-
-    toggleInfoModal = () => {
+  guess(guess) {
+    guess = parseInt(guess, 10);
+    if (isNaN(guess)) {
       this.setState({
-          modalIsOpen: !this.state.modalIsOpen
+        feedback: "Please enter a valid number"
       });
+      return;
     }
 
-    render() {
-        return (
-            <div>
-                <InfoModal
-                    isOpen={this.state.modalIsOpen}
-                    onAfterOpen={this.afterOpenModal}
-                    onRequestClose={this.toggleInfoModal}
-                    contentLabel="What do I do?"
-              />
-                  <Header onNewGame={() => this.newGame()} toggleInfoModal={this.toggleInfoModal} />
-                  <GuessSection feedback={this.state.feedback}
-                      onGuess={(guess) => this.guess(guess)} />
-                  <StatusSection count={this.state.guesses.length} 
-                      guesses={this.state.guesses} />
+    const difference = Math.abs(guess - this.state.correctAnswer);
 
-            </div>
-        );
+    let feedback;
+    if (difference >= 50) {
+      feedback = "You're Ice Cold...";
+    } else if (difference >= 30) {
+      feedback = "You're Cold...";
+    } else if (difference >= 10) {
+      feedback = "You're Warm";
+    } else if (difference >= 1) {
+      feedback = "You're Hot!";
+    } else {
+      feedback = "You got it!";
     }
+
+    this.setState({
+      feedback,
+      guesses: [...this.state.guesses, guess]
+    });
+
+    document.title = feedback ? `${feedback} | Hot or Cold` : "Hot or Cold";
+  }
+
+  toggleInfoModal = () => {
+    this.setState({
+      modalIsOpen: !this.state.modalIsOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <InfoModal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.toggleInfoModal}
+          contentLabel="What do I do?"
+        />
+        <Header
+          onNewGame={() => this.newGame()}
+          toggleInfoModal={this.toggleInfoModal}
+        />
+        <GuessSection
+          feedback={this.state.feedback}
+          onGuess={guess => this.guess(guess)}
+        />
+        <StatusSection
+          count={this.state.guesses.length}
+          guesses={this.state.guesses}
+        />
+      </div>
+    );
+  }
 }
-
--- src/components/status-section.js
+++ src/components/status-section.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './guess-count.css';
-import './guess-list.css';
+import "./guess-count.css";
+import "./guess-list.css";
 
 export default function guessSection(props) {
-    const guesses = props.guesses.map((guess, index) => (
-        <li aria-label={`${guess}`} key={index}>
-            <span className="visually-hidden">Guess #{index + 1} was </span>{guess}
-        </li>
-    ));
-    return (
-        <section role="region"
-        aria-label="Game status" aria-describedby="guessCount">
-            <p id="guessCount">
-                You've made <span id="count">{props.count}</span> guesses!
-            </p>
-            <ul  id="guessList" className="guessBox clearfix">
-                {guesses}
-            </ul>
-        </section>
-    )
+  const guesses = props.guesses.map((guess, index) =>
+    <li aria-label={`${guess}`} key={index}>
+      <span className="visually-hidden">
+        Guess #{index + 1} was{" "}
+      </span>   
+      {guess}
+    </li>
+  );
+  return (
+    <section role="region"
+      aria-label="Game status"
+      aria-describedby="guessCount"
+    >
+      <p id="guessCount">
+        You've made <span id="count">{props.count}</span> guesses!
+      </p>
+      <ul id="guessList" className="guessBox clearfix">
+        {guesses}
+      </ul>
+    </section>
+  );
 }
-
-
