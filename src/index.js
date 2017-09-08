63b652827a2cc80d041bbd997f1dd5dd56b981af
Elias Mason <eliasjmason@gmail.com>
2017-09-07 22:08:22 -0700

    Comment out reset css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import './reset.css';
+// import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './reset.css';
 import './index.css';
 
 import Game from './components/game';
