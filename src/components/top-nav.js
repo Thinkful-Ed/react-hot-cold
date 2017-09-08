19d293c1fff318c1492543bb95ff528117e9b6d3
Elias Mason <eliasjmason@gmail.com>
2017-09-07 22:23:53 -0700

    Prettier

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,37 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import './top-nav.css';
+import "./top-nav.css";
 
 export default class TopNav extends React.Component {
-    onNewGame(event) {
-        if (this.props.onNewGame) {
-            this.props.onNewGame();
-        }
+  onNewGame(event) {
+    if (this.props.onNewGame) {
+      this.props.onNewGame();
     }
+  }
 
-    onInfo(event) {
-        event.preventDefault();
-        if (this.props.onInfo) {
-            this.props.onInfo();
-        }
+  onInfo(event) {
+    event.preventDefault();
+    if (this.props.onInfo) {
+      this.props.onInfo();
     }
+  }
 
-    render() {
-        return (
-            <nav>
-                <ul className="clearfix">
-                    <li>
-                        <a className="what" aria-label="How to play" href="#modal" onClick={e => this.onInfo(e)}>
-                            What?
-                        </a>
-                    </li>
-                    <li>
-                        <a className="new" aria-live="Start a new game" href="#feedback" onClick={e => this.onNewGame(e)}>
-                            + New Game
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
-};
+  render() {
+    return (
+      <nav>
+        <ul className="clearfix">
+          <li>
+            <a
+              className="what"
+              aria-label="How to play"
+              href="#modal"
+              onClick={e => this.onInfo(e)}
+            >
+              What?
+            </a>
+          </li>
+          <li>
+            <a
+              className="new"
+              aria-live="Start a new game"
+              href="#feedback"
+              onClick={e => this.onNewGame(e)}
+            >
+              + New Game
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
