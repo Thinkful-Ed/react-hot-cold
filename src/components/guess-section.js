acfeae0ff81acafe678f8a564ad41d84d58300ec
Elias Mason <eliasjmason@gmail.com>
2017-09-10 23:16:17 -0700

    Convert to functional component

--- src/components/guess-section.js
+++ src/components/guess-section.js
@@ -5,17 +5,14 @@ import GuessForm from "./guess-form";
 
 import "./guess-section.css";
 
-export default class GuessSection extends React.Component {
-
-  render() {
-    return (
-      <section role="region"
-        aria-label="Guess section"
-        aria-describedby="feedback"
-      >
-        <Feedback feedback={this.props.feedback}/>
-        <GuessForm makeGuess={guess => this.props.makeGuess(guess)}/>
-      </section>
-    );
-  }
+export default function GuessSection(props){
+  return (
+    <section role="region"
+      aria-label="Guess section"
+      aria-describedby="feedback"
+    >
+      <Feedback feedback={props.feedback}/>
+      <GuessForm makeGuess={guess => props.makeGuess(guess)}/>
+    </section>
+  );
 }
