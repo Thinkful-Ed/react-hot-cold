86b48e96fe007317fa1ff034b7720a4913140c71
Elias Mason <eliasjmason@gmail.com>
2017-09-10 21:40:35 -0700

    Set gameWon to false

--- src/components/game.js
+++ src/components/game.js
@@ -12,7 +12,7 @@ export default class Game extends React.Component {
       guesses: [],
       feedback: "Make your guess!",
       correctAnswer: Math.round(Math.random() * 100),
-      gameWon: true
+      gameWon: false
     };
   }
 
