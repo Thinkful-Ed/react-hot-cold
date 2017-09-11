c05a3d90780101a1191e99068cdc5b0a8542d4d9
Elias Mason <eliasjmason@gmail.com>
2017-09-11 14:18:08 -0700

    Simpler return structure

--- src/components/aural-status.js
+++ src/components/aural-status.js
@@ -2,13 +2,9 @@ import React from "react";
 
 export default function AuralStatus(props) {
   return (
-    <div 
-      id="status-readout"
-      className="visuallyhidden"
-      aria-live="assertive"
-      aria-atomic="true"
-    >
-      <p> {props.statusText}</p>
-    </div>
+    <p id="status-readout"
+    className="visuallyhidden"
+    aria-live="assertive"
+    aria-atomic="true"> {props.statusText }</p>
   );
 }
