0cbfc8545d63b2d2ab71cfd656e30781d758daeb
Elias Mason <eliasjmason@gmail.com>
2017-09-11 17:11:19 -0700

    Use key to force re-read of feedback on each guess

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 export default function Feedback(props) {
+    /** 
+     * Generate a random key so that React treats the feedback message 
+     * as a DOM change, even when a guess does not change the feedback.
+     * This is necessary for consistent aural feedback via aria-live.
+    */
+    const key = Math.random();
     return (
         <h2 id="feedback"
         role="status"
         aria-live="assertive"
-        aria-atomic="true">
+        aria-atomic="true"
+        key={key}>
           {props.feedback} {!props.gameWon ? <span className="visuallyhidden">Guess again!</span>: ''}
         </h2>
     )
