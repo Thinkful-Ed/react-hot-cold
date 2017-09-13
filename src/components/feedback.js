1370c7c2bc3948738b4eaa094d796ec60aa20f01
Elias Mason <eliasjmason@gmail.com>
2017-09-12 18:03:11 -0700

    Prettier

--- src/components/feedback.js
+++ src/components/feedback.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 export default function Feedback(props) {
-    /** 
-     * Generate a random key so that React treats the feedback message 
-     * as a DOM change, even when a guess does not change the feedback.
-     * This is necessary for consistent aural feedback via aria-live.
-    */
-    const key = Math.random();
-    return (
-        <h2 id="feedback"
-        role="status"
-        aria-live="assertive"
-        aria-atomic="true"
-        key={key}>
-          {props.feedback} {!props.gameWon ? <span className="visuallyhidden">Guess again!</span>: ''}
-        </h2>
-    )
+  /** 
+   * Generate a random key so that React treats the feedback message 
+   * as a DOM change, even when a guess does not change the feedback.
+   * This is necessary for consistent aural feedback via aria-live.
+  */
+  const key = Math.random();
+  return (
+    <h2 id="feedback"
+    role="status"
+    aria-live="assertive"
+    aria-atomic="true"
+    key={key}>
+        {props.feedback} {!props.gameWon ? <span className="visuallyhidden">Guess again!</span>: ''}
+    </h2>
+  )
 }
