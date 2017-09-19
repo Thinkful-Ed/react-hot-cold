e97b2dcaadfff584b564f47b80b33ba8485abfbf
Elias Mason <eliasjmason@gmail.com>
2017-09-18 21:18:25 -0700

    Prettier; better const name

--- src/components/guess-list.js
+++ src/components/guess-list.js
@@ -3,18 +3,16 @@ import React from 'react';
 // import "./guess-list.css";
 
 export default function GuessList(props) {
-  const guesses = props.guesses.map((guess, index) =>
-  <li aria-label={`${guess}`} key={index}>
-    <span className="visuallyhidden">
-      Guess #{index + 1} was{" "}
-    </span>   
-    {guess}
-  </li>
-);
+  const guessesHTML = props.guesses.map((guess, index) => (
+    <li aria-label={`${guess}`} key={index}>
+      <span className="visuallyhidden">Guess #{index + 1} was </span>
+      {guess}
+    </li>
+  ));
 
   return (
     <ul id="guessList" className="guessBox clearfix">
-      {guesses}
+      {guessesHTML}
     </ul>
   );
 }
