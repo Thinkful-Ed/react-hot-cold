5faea978e29c87b8b76ef025b5cf6cc6be14ba55
Elias Mason <eliasjmason@gmail.com>
2017-09-18 21:16:34 -0700

    Statustext --> auralStatus for consistency

--- src/components/status-section.js
+++ src/components/status-section.js
@@ -7,14 +7,14 @@ import AuralStatus from './aural-status';
 import './status-section.css';
 
 export default function guessSection(props) {
-  const { guesses, statusText } = props;
+  const { guesses, auralStatus } = props;
   const guessCount = guesses.length;
 
   return (
     <section aria-labelledby="guessCount" aria-describedby="guessList">
       <GuessCount guessCount={guessCount} />
       <GuessList guesses={guesses} />
-      <AuralStatus statusText={statusText} />
+      <AuralStatus auralStatus={auralStatus} />
     </section>
   );
 }
