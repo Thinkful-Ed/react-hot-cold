a91b929ff6c81fd8c7b23db47a99ecce58dbb9c6
Elias Mason <eliasjmason@gmail.com>
2017-10-05 23:44:30 -0700

    Add info section instructions on the state of game link

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -12,6 +12,7 @@ export default function InfoSection(props) {
           <li>You need to <strong>guess</strong> until you can find the hidden secret number.</li>
           <li>You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
         </ol>
+        <p className="visuallyhidden">At any time, you can use the "Hear state of gane" link to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
     </section>
   );
 }
