2e25c7f18da83ac6d0c0512e6608c4d9d33c2ad9
Elias Mason <eliasjmason@gmail.com>
2017-10-28 03:27:01 -0700

    Single-quote, escape entity

--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import "./guess-count.css";
+import './guess-count.css';
 
 export default function GuessCount(props) {
   return (
     <h2 id="guessCount">
-      You've made <span id="count">{props.guessCount}</span> guesses!
+      You&apos;ve made <span id="count">{props.guessCount}</span> guesses!
     </h2>
   );
 }
