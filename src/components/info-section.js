fa9c91c7d35246983e1721443cb97f32dbba8744
Elias Mason <eliasjmason@gmail.com>
2017-11-16 03:16:34 -0800

    Style cleanup & literal html entities

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -10,9 +10,9 @@ export default function InfoSection(props) {
       <ol>
         <li>I pick a <strong>random secret number</strong> between 1 to 100 and keep it hidden.</li>
         <li>You need to <strong>guess</strong> until you can find the hidden secret number.</li>
-        <li>You will <strong>get feedback</strong> on how close (&quot;hot&quot;) or far (&quot;cold&quot;) your guess is.</li>
+        <li>You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
       </ol>
-      <p>If you&apos;re using a screen reader, you can use the visually hidden &quot;Hear state of game&quot; link in the navigation region to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
+      <p>If you're using a screen reader, you can use the visually hidden "Hear state of game" link in the navigation region to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
     </section>
   );
 }
